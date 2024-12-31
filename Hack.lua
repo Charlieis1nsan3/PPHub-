0da95c80b5ab7e6c08a8a943b5da4806305c502a
HyperNut <charliemorrison960@gmail.com>
2024-12-31 08:36:06 -0500

    Update and rename Hack to Hack.lua

---
+++ Hack.lua
@@ -0,0 +1,149 @@
+while true do
+    -- Run each piece of code concurrently
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").LocalPlayer.Character.RightHand,
+        [2] = game:GetService("Players").LocalPlayer.Character.RightLowerArm,
+        [3] = game:GetService("Players").LocalPlayer.Character.RightUpperArm,
+        [4] = game:GetService("Players").LocalPlayer.Character.UpperTorso,
+        [5] = game:GetService("Players").LocalPlayer.Character.LeftHand,
+        [6] = workspace.Train:FindFirstChild("3"):FindFirstChild("6010").HitBox
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+    end)
+
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").LocalPlayer.Character.RightHand,
+        [2] = game:GetService("Players").LocalPlayer.Character.LeftUpperArm,
+        [3] = game:GetService("Players").LocalPlayer.Character.RightLowerArm,
+        [4] = game:GetService("Players").LocalPlayer.Character.LeftLowerArm,
+        [5] = game:GetService("Players").LocalPlayer.Character.RightUpperArm,
+        [6] = game:GetService("Players").LocalPlayer.Character.UpperTorso,
+        [7] = game:GetService("Players").LocalPlayer.Character.LeftHand,
+        [8] = game:GetService("Players").LocalPlayer.Character.LowerTorso,
+        [9] = workspace.Train:FindFirstChild("3"):FindFirstChild("6011").HitBox
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+
+    end)
+
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").LocalPlayer.Character.RightHand,
+        [2] = game:GetService("Players").LocalPlayer.Character.RightLowerArm,
+        [3] = game:GetService("Players").LocalPlayer.Character.LeftLowerArm,
+        [4] = game:GetService("Players").LocalPlayer.Character.RightUpperArm,
+        [5] = game:GetService("Players").LocalPlayer.Character.UpperTorso,
+        [6] = game:GetService("Players").LocalPlayer.Character.LeftHand,
+        [7] = workspace.Train:FindFirstChild("3"):FindFirstChild("6011").HitBox,
+        [8] = game:GetService("Players").Taiwan0018.Character.RightLowerArm,
+        [9] = game:GetService("Players").Taiwan0018.Character.UpperTorso,
+        [10] = game:GetService("Players").Taiwan0018.Character.Head,
+        [11] = game:GetService("Players").Taiwan0018.Character.RightUpperArm,
+        [12] = game:GetService("Players").Taiwan0018.Character.HumanoidRootPart,
+        [13] = game:GetService("Players").Taiwan0018.Character.LeftUpperArm,
+        [14] = game:GetService("Players").Taiwan0018.Character.LeftLowerArm
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+    end)
+
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").LocalPlayer.Character.RightHand,
+        [2] = game:GetService("Players").LocalPlayer.Character.RightLowerArm,
+        [3] = game:GetService("Players").LocalPlayer.Character.LeftLowerArm,
+        [4] = game:GetService("Players").LocalPlayer.Character.RightUpperArm,
+        [5] = game:GetService("Players").LocalPlayer.Character.UpperTorso,
+        [6] = game:GetService("Players").LocalPlayer.Character.LeftHand,
+        [7] = workspace.Train:FindFirstChild("3"):FindFirstChild("6011").HitBox,
+        [8] = game:GetService("Players").Taiwan0018.Character.RightLowerArm,
+        [9] = game:GetService("Players").Taiwan0018.Character.UpperTorso,
+        [10] = game:GetService("Players").Taiwan0018.Character.Head,
+        [11] = game:GetService("Players").Taiwan0018.Character.RightUpperArm,
+        [12] = game:GetService("Players").Taiwan0018.Character.HumanoidRootPart,
+        [13] = game:GetService("Players").Taiwan0018.Character.LeftUpperArm,
+        [14] = game:GetService("Players").Taiwan0018.Character.LeftLowerArm
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+
+    end)
+
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").LocalPlayer.Character.RightHand,
+        [2] = game:GetService("Players").LocalPlayer.Character.LeftUpperArm,
+        [3] = game:GetService("Players").LocalPlayer.Character.RightLowerArm,
+        [4] = game:GetService("Players").LocalPlayer.Character.LeftLowerArm,
+        [5] = game:GetService("Players").LocalPlayer.Character.RightUpperArm,
+        [6] = game:GetService("Players").LocalPlayer.Character.UpperTorso,
+        [7] = game:GetService("Players").LocalPlayer.Character.LeftHand,
+        [8] = game:GetService("Players").LocalPlayer.Character.LowerTorso,
+        [9] = workspace.Train:FindFirstChild("3"):FindFirstChild("6009").HitBox
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+    end)
+
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").LocalPlayer.Character.RightHand,
+        [2] = game:GetService("Players").LocalPlayer.Character.LeftUpperArm,
+        [3] = game:GetService("Players").LocalPlayer.Character.RightLowerArm,
+        [4] = game:GetService("Players").LocalPlayer.Character.LeftLowerArm,
+        [5] = game:GetService("Players").LocalPlayer.Character.RightUpperArm,
+        [6] = game:GetService("Players").LocalPlayer.Character.UpperTorso,
+        [7] = game:GetService("Players").LocalPlayer.Character.LeftHand,
+        [8] = game:GetService("Players").LocalPlayer.Character.LowerTorso,
+        [9] = workspace.Train:FindFirstChild("3"):FindFirstChild("6009").HitBox
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+    end)
+
+    task.spawn(function()
+        local args = {
+    [1] = false,
+    [2] = {
+        [1] = game:GetService("Players").irex61520.Character.RightUpperArm,
+        [2] = game:GetService("Players").irex61520.Character.UpperTorso,
+        [3] = game:GetService("Players").irex61520.Character.HumanoidRootPart,
+        [4] = game:GetService("Players").irex61520.Character.LeftUpperArm,
+        [5] = game:GetService("Players").irex61520.Character.Head,
+        [6] = workspace.Train:FindFirstChild("3"):FindFirstChild("6010").HitBox,
+        [7] = game:GetService("Players").irex61520.Character.LeftLowerArm
+    }
+}
+
+game:GetService("ReplicatedStorage").Remotes.Skill.ServerBehaviour:FireServer(unpack(args))
+    end)
+
+    task.spawn(function()
+          sendMessage("\PPHub is active 😈 (made with love by charlie/")
+    end)
+
+       wait(0.1)
+end
+
